d2297e3f1d2eaea62911012ec4e6667d5d0cd812
Zucker, Jeremy D <jeremy.zucker@pnnl.gov>
2025-06-27 10:27:52 -0700

    Renamed tests to what they were actually testing. Replaced deprecated eqnarray with align

--- tests/test_crop_example_SBML.py
+++ tests/test_crop_example_SBML.py
@@ -11,16 +11,16 @@ def load_model():
     return cobra.io.read_sbml_model(path.joinpath("example_model.xml").__str__())
 
 
-def test_no_grow_e():
-    # B + E  nutrient condition is no growth
-    model = cobra.io.read_sbml_model(path.joinpath("example_model.xml").__str__())
+def test_e_is_no_growth():
+    # E  nutrient condition is no growth
+    model = load_model()
     media = {"EX_E_e": 100}
     model.medium = media
     obj_func = model.slim_optimize()
     assert obj_func == 0.0
 
 
-def test_no_grow_a():
+def test_a_alone_is_growth():
     # A alone is growth
     model = cobra.io.read_sbml_model(path.joinpath("example_model.xml").__str__())
     media = {"EX_A_e": 100}
@@ -29,7 +29,7 @@ def test_no_grow_a():
     assert obj_func == 100.0
 
 
-def test_no_grow_4():
+def test_a_and_v3_ko_is_growth():
     # A + v3 knockout is no-growth
     model = cobra.io.read_sbml_model(path.joinpath("example_model.xml").__str__())
     media = {"EX_A_e": 100}
@@ -40,11 +40,11 @@ def test_no_grow_4():
     assert obj_func == 50.0
 
 
-def test_no_grow_5():
+def test_a_and_e_and_v3_ko_is_growth():
     # A + E + v3 knockout is growth
     model = cobra.io.read_sbml_model(path.joinpath("example_model.xml").__str__())
     media = {"EX_A_e": 100, "EX_E_e": 100}
     model.remove_reactions(model.reactions.get_by_id("R_A_to_C"))
     model.medium = media
     obj_func = model.slim_optimize()
-    assert obj_func > 0.0
+    assert obj_func > 50.0
